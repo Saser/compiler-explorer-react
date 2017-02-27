909e3de89f3470c16943a4f71ea9263af643074b
Christian Persson <saser@live.se>
2017-02-27 15:16:39 +0100

    Add a sample tree that might correspond to a ModLang expression

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,25 @@ import React from 'react';
 
 import LangTree from './LangTree.js'
 
-const sampleTree = {};
-const sampleLang = 'derp';
+const sampleTree = {
+    "con": "Tdec",
+    "trace": [1, 1, 1, 16],
+    "dec": {
+        "con": "Dlet",
+        "trace": [1, 1, 1, 15],
+        "pat": {
+            "con": "Pvar",
+            "trace": [1, 5, 1, 5],
+            "varn": "\"y\"",
+        },
+        "expr": {
+            "con": "Lit",
+            "trace": [1, 9, 1, 5],
+            "val": "\"hello\"",
+        },
+    },
+}
+const sampleLang = 'mod';
 
 const App = () => (
     <LangTree tree={sampleTree} lang={sampleLang} />
