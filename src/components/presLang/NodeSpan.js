66381b32230a941f2bf18212ab3bae79f92f9440
Christian Persson <saser@live.se>
2017-03-01 16:35:17 +0100

    Create NodeSpan, basically equivalent to previous TraceSpan

---
+++ src/components/presLang/NodeSpan.js
@@ -0,0 +1,26 @@
+import React, { PropTypes } from 'react';
+
+const NodeSpan = ({ isHighlighted, onClick, children }) => {
+    const onClickWithoutPropagation = (event) => {
+        event.stopPropagation();
+        onClick();
+    }
+    return (
+        <span
+            onClick={onClickWithoutPropagation}
+            style={{
+                background: isHighlighted ? '#09cdda' : 'none,'
+            }}
+        >
+            {children}
+        </span>
+    );
+}
+
+NodeSpan.propTypes = {
+    isHighlighted: PropTypes.bool.isRequired,
+    onClick: PropTypes.func.isRequired,
+    children: PropTypes.array.isRequired,
+};
+
+export default NodeSpan;
