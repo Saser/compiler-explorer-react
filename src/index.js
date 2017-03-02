060a4378c5c319472730a87108b7487cfa8289e9
Christian Persson <saser@live.se>
2017-03-02 16:11:07 +0100

    Enable Redux Dev Tools extension

--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import compilerExplorer from './reducers/compilerExplorer.js'
 
-const store = createStore(compilerExplorer)
+const store = createStore(
+    compilerExplorer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
     <Provider store={store}>
