f83c314bc230c73ed666a46e17aa866d6f83adab
Christian Persson <saser@live.se>
2017-03-02 16:13:12 +0100

    Refactor to look a bit nicer

--- src/components/presLang/Tdec.js
+++ src/components/presLang/Tdec.js
@@ -3,11 +3,14 @@ import React, { PropTypes } from 'react';
 import NodeSpan from './NodeSpan.js';
 import PresLangTree from './PresLangTree.js'
 
-const Tdec = ({ dec, isHighlighted, onClick, dispatchFunction }) => (
-    <NodeSpan isHighlighted={isHighlighted} onClick={onClick}>
-        Tdec (<PresLangTree tree={dec} onTreeClick={dispatchFunction} />)
-    </NodeSpan>
-)
+const Tdec = ({ dec, isHighlighted, onClick, dispatchFunction }) => {
+    const decTree = <PresLangTree tree={dec} onTreeClick={dispatchFunction} />;
+    return (
+        <NodeSpan isHighlighted={isHighlighted} onClick={onClick}>
+            Tdec ({decTree})
+        </NodeSpan>
+    );
+}
 
 Tdec.propTypes = {
     dec: PropTypes.object.isRequired,
