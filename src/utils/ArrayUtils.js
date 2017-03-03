b28b40c74fc9430762408d4e29d106b36f833217
Christian Persson <saser@live.se>
2017-03-03 11:38:28 +0100

    YUSSS

--- src/utils/ArrayUtils.js
+++ src/utils/ArrayUtils.js
@@ -3,5 +3,16 @@ export const isArrayPrefix = (prefix, arr) => {
 }
 
 export const arrayEquals = (arr1, arr2) => {
-    return false;
+    // If both arrays are empty, then they are equal.
+    if (arr1.length === 0 && arr2.length === 0) {
+        return true;
+    }
+
+    // Since we know that at least one of the arrays is non-empty, of the other
+    // is empty, they are not equal.
+    if (arr1.length === 0 || arr2.length === 0) {
+        return false;
+    }
+
+    return Object.is(arr1[0], arr2[0]) && arrayEquals(arr1.slice(1), arr2.slice(1));
 }
