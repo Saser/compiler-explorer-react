dad0b37ab93db9bfe8b4ef73e95ecfa1dc473440
Christian Persson <saser@live.se>
2017-03-23 13:28:37 +0100

    Implement containsSubtrace

--- src/logic/HighlightTrace.js
+++ src/logic/HighlightTrace.js
@@ -1,3 +1,33 @@
+import { traceEquals } from '../utils/TraceUtils.js';
+
 export const containsSubtrace = (sub, trace) => {
-    return undefined;
+    if (sub === undefined) {
+        throw 'sub is undefined';
+    }
+
+    if (trace === undefined) {
+        throw 'trace is undefined';
+    }
+
+    if (sub === null) {
+        return true;
+    }
+
+    return containsSubtraceAux(sub, trace);
+}
+
+const containsSubtraceAux = (sub, trace) => {
+    if (traceEquals(sub, trace)) {
+        return true;
+    }
+
+    if (trace === null) {
+        return false;
+    }
+
+    if (trace.cons === 'Cons') {
+        return containsSubtraceAux(sub, trace.trace);
+    } else {
+        return containsSubtraceAux(sub, trace.trace1) || containsSubtraceAux(sub, trace.trace2);
+    }
 }
