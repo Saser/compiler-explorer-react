3c0a38f6efef11076dd8c1cdcbdb2c8045758d9d
Christian Persson <saser@live.se>
2017-03-23 18:06:32 +0100

    Add new action types, for compiling source code
    These are just placeholders for JSON.parse at the moment. They will be handy
    later on, though, when actual server-side compilation takes place.

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,3 +1,9 @@
 // Handling the activated trace.
 export const TRACE_ACTIVATED = 'TRACE_ACTIVATED';
 export const TRACE_DEACTIVATED = 'TRACE_DEACTIVATED';
+
+// Handling the user input, i.e. compile code.
+// TODO: currently, this means parsing the pasted JSON.
+export const SOURCE_CODE_UPDATED = 'SOURCE_CODE_UPDATED';
+export const SOURCE_CODE_REQUEST_COMPILE = 'SOURCE_CODE_REQUEST_COMPILE';
+export const SOURCE_CODE_COMPILE_SUCCESS = 'SOURCE_CODE_COMPILE_SUCCESS';
