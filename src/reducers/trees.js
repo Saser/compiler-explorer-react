737c1fbf2f4288575f9f1303182583f2388b371f
Christian Persson <saser@live.se>
2017-03-23 19:29:57 +0100

    Fix starting state of `trees`

--- src/reducers/trees.js
+++ src/reducers/trees.js
@@ -1,22 +1,25 @@
 import { constructSimpleTrace } from '../utils/TraceUtils.js';
 
 const initialTrees = {
-    "lang": "mod",
-    "tree": {
-        "con": "Tdec",
-        "trace": constructSimpleTrace([1, 1, 1, 16]),
-        "dec": {
-            "con": "Dlet",
-            "trace": constructSimpleTrace([1, 1, 1, 15]),
-            "pat": {
-                "con": "Pvar",
-                "trace": constructSimpleTrace([1, 5, 1, 5]),
-                "varn": "\"y\"",
-            },
-            "expr": {
-                "con": "Lit",
-                "trace": constructSimpleTrace([1, 9, 1, 5]),
-                "val": "\"hello\"",
+    isParsing: false,
+    trees: {
+        "lang": "mod",
+        "tree": {
+            "con": "Tdec",
+            "trace": constructSimpleTrace([1, 1, 1, 16]),
+            "dec": {
+                "con": "Dlet",
+                "trace": constructSimpleTrace([1, 1, 1, 15]),
+                "pat": {
+                    "con": "Pvar",
+                    "trace": constructSimpleTrace([1, 5, 1, 5]),
+                    "varn": "\"y\"",
+                },
+                "expr": {
+                    "con": "Lit",
+                    "trace": constructSimpleTrace([1, 9, 1, 5]),
+                    "val": "\"hello\"",
+                },
             },
         },
     },
