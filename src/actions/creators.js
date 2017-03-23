0ea545d1596c0a00fd78e382b1d75b303767628e
Christian Persson <saser@live.se>
2017-03-23 18:14:17 +0100

    Add creators for the new action types

--- src/actions/creators.js
+++ src/actions/creators.js
@@ -12,3 +12,23 @@ export const traceDeactivated = () => {
         type: types.TRACE_DEACTIVATED,
     };
 }
+
+export const sourceCodeUpdated = (sourceCode) => {
+    return {
+        type: types.SOURCE_CODE_UPDATED,
+        sourceCode,
+    };
+}
+
+export const sourceCodeRequestCompile = () => {
+    return {
+        type: types.SOURCE_CODE_REQUEST_COMPILE,
+    };
+}
+
+export const sourceCodeCompileSuccess = (json) => {
+    return {
+        type: types.SOURCE_CODE_COMPILE_SUCCESS,
+        json,
+    };
+}
