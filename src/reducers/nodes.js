572e0036a12e332fdb5c3ea612b3ca039a0c174f
Christian Persson <saser@live.se>
2017-03-23 18:04:43 +0100

    Refactor to use new trace type (object instead of array)

--- src/reducers/nodes.js
+++ src/reducers/nodes.js
@@ -1,17 +1,19 @@
+import { constructSimpleTrace } from '../utils/TraceUtils.js';
+
 const initialNodes = {
     "con": "Tdec",
-    "trace": [1, 1, 1, 16],
+    "trace": constructSimpleTrace([1, 1, 1, 16]),
     "dec": {
         "con": "Dlet",
-        "trace": [1, 1, 1, 15],
+        "trace": constructSimpleTrace([1, 1, 1, 15]),
         "pat": {
             "con": "Pvar",
-            "trace": [1, 5, 1, 5],
+            "trace": constructSimpleTrace([1, 5, 1, 5]),
             "varn": "\"y\"",
         },
         "expr": {
             "con": "Lit",
-            "trace": [1, 9, 1, 5],
+            "trace": constructSimpleTrace([1, 9, 1, 5]),
             "val": "\"hello\"",
         },
     },
