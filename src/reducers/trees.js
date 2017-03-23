f6ef3d8551d94b07e8f03242e937b4bb0e5f26ff
Christian Persson <saser@live.se>
2017-03-23 19:35:37 +0100

    Handle the trees actions in the `trees` reducer

--- src/reducers/trees.js
+++ src/reducers/trees.js
@@ -1,3 +1,8 @@
+import {
+    TREES_PARSING_STARTED,
+    TREES_PARSING_FINISHED,
+} from '../actions/types.js';
+
 import { constructSimpleTrace } from '../utils/TraceUtils.js';
 
 const initialTrees = {
@@ -26,7 +31,21 @@ const initialTrees = {
 };
 
 const trees = (state = initialTrees, action) => {
-    return state;
+    switch (action.type) {
+        case TREES_PARSING_STARTED:
+            return {
+                ...state,
+                isParsing: true,
+            };
+        case TREES_PARSING_FINISHED:
+            return {
+                ...state,
+                isParsing: false,
+                trees: action.trees,
+            };
+        default:
+            return state;
+    }
 }
 
 export default trees;
