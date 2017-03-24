cce96116971e73e770649fd4b3a6ee6ee66350d3
Christian Persson <saser@live.se>
2017-03-24 17:00:16 +0100

    Add tests and skeleton for `addIntegerKeys`

---
+++ src/utils/ReactUtils.js
@@ -0,0 +1,6 @@
+// Returns a new array of new objects, each having a `key` property with its
+// value set to the index of that object. Does not mutate the original array or
+// the objects in that array.
+export const addIntegerKeys = (objs) => {
+    return undefined;
+}
---
+++ src/utils/ReactUtils.test.js
@@ -0,0 +1,40 @@
+import { addIntegerKeys } from './ReactUtils.js';
+
+describe('addIntegerKeys', () => {
+    it('returns an empty array for empty array as input', () => {
+        expect(addIntegerKeys([])).toEqual([]);
+    });
+
+    it('returns a single-element array for single-element array as input, having a `key` property with value 0', () => {
+        const obj = {
+            prop1: 'hello',
+            prop2: 'world',
+            nested: {
+                nesting: 'is fun',
+            },
+        };
+        const arr = [obj];
+
+        const expectedObj = {
+            ...obj,
+            key: 0,
+        };
+        const expectedArr = [expectedObj];
+
+        expect(addIntegerKeys(arr)).toEqual(expectedArr);
+    });
+
+    it('returns the same array with `key`s being 0, 1, 2 for 3-element array', () => {
+        const obj1 = { a: 1 };
+        const obj2 = { b: 2 };
+        const obj3 = { c: 3 };
+        const arr = [obj1, obj2, obj3];
+
+        const expectedObj1 = { ...obj1, key: 0 };
+        const expectedObj2 = { ...obj2, key: 1 };
+        const expectedObj3 = { ...obj3, key: 2 };
+        const expectedArr = [expectedObj1, expectedObj2, expectedObj3];
+
+        expect(addIntegerKeys(arr)).toEqual(expectedArr);
+    });
+});
