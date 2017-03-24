53a6df12f7c9a15a65f81b3ebd21317663b48cf0
Christian Persson <saser@live.se>
2017-03-24 21:26:43 +0100

    Add some TODOs, so I do not forget to fix them

--- src/containers/LangTrees.js
+++ src/containers/LangTrees.js
@@ -3,11 +3,13 @@ import { connect } from 'react-redux';
 import PresLangTreeWrapper from '../components/preslang/PresLangTreeWrapper.js';
 
 const mapStateToProps = (state) => ({
+    // TODO: the below is hardcoded, and should be changed in the future.
     tree: state.trees.trees && state.trees.trees[0],
 })
 
 const mapDispatchToProps = (dispatch) => ({
     onClickFactory: (trace) => {
+        // TODO: replace this with actual dispatch stuff.
         return () => console.log(trace);
     },
 })
