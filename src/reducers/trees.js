ba9b7fd76da089f29a9a236ceb15ae5dbbfc80c8
Christian Persson <saser@live.se>
2017-03-24 20:43:54 +0100

    Remove unused import

--- src/reducers/trees.js
+++ src/reducers/trees.js
@@ -3,8 +3,6 @@ import {
     TREES_PARSING_FINISHED,
 } from '../actions/types.js';
 
-import { constructSimpleTrace } from '../utils/TraceUtils.js';
-
 const initialTrees = {
     isParsing: false,
     trees: null,
