f55cae6cbc793cb5e2874cf5c79452c7e3b71b0a
Christian Persson <saser@live.se>
2017-03-24 16:45:38 +0100

    Tests for `rangeInc`

--- src/utils/ArrayUtils.test.js
+++ src/utils/ArrayUtils.test.js
@@ -20,7 +20,7 @@ describe('range', () => {
         expect(range(0, 1)).toHaveLength(1 - 0); // === 1
         expect(range(-1, 1)).toHaveLength(1 - (-1)); // === 2
         expect(range(0, 10)).toHaveLength(10 - 0); // === 10
-        expect(range(-10, -5)).toHaveLength(-5 - (-10)) // === 5;
+        expect(range(-10, -5)).toHaveLength(-5 - (-10)); // === 5;
     });
 
     it('always includes `start` as element if non-empty', () => {
@@ -31,7 +31,7 @@ describe('range', () => {
         expect(range(-10, -9)).toContain(-10);
     });
 
-    it('never includes `end` as en element', () => {
+    it('never includes `end` as an element', () => {
         expect(range(0, 0)).not.toContain(0);
         expect(range(0, 1)).not.toContain(1);
         expect(range(1, 1)).not.toContain(1);
@@ -39,5 +39,49 @@ describe('range', () => {
         expect(range(-10, 10)).not.toContain(10);
         expect(range(-10, 0)).not.toContain(0);
         expect(range(-10, -10)).not.toContain(-10);
+    });
+});
+
+import { rangeInc } from './ArrayUtils.js';
+
+describe('rangeInc', () => {
+    it('returns single-element array on `start` === `end`', () => {
+        expect(rangeInc(0, 0)).toEqual([0]);
+        expect(rangeInc(1, 1)).toEqual([1]);
+        expect(rangeInc(-1, -1)).toEqual([-1]);
+    });
+
+    it('returns empty array when `start` > `end`', () => {
+        expect(rangeInc(1, 0)).toEqual([]);
+        expect(rangeInc(1, -1)).toEqual([]);
+        expect(rangeInc(0, -1)).toEqual([]);
+        expect(rangeInc(-1, -2)).toEqual([]);
+    });
+
+    it('returns (`end` - `start`) + 1 elements', () => {
+        expect(rangeInc(0, 0)).toHaveLength(0 - 0 + 1); // === 1
+        expect(rangeInc(10, 10)).toHaveLength(10 - 10 + 1); // === 1
+        expect(rangeInc(0, 1)).toHaveLength(1 - 0 + 1); // === 2
+        expect(rangeInc(-1, 1)).toHaveLength(1 - (-1) + 1); // === 3
+        expect(rangeInc(0, 10)).toHaveLength(10 - 0 + 1); // === 11
+        expect(rangeInc(-10, -5)).toHaveLength(-5 - (-10) + 1); // === 6
+    });
+
+    it('always includes `start` as element if non-empty', () => {
+        expect(rangeInc(0, 1)).toContain(0);
+        expect(rangeInc(0, 10)).toContain(0);
+        expect(rangeInc(9, 10)).toContain(9);
+        expect(rangeInc(-10, 0)).toContain(-10);
+        expect(rangeInc(-10, -9)).toContain(-10);
+    });
+
+    it('always includes `end` as an element', () => {
+        expect(rangeInc(0, 0)).toContain(0);
+        expect(rangeInc(0, 1)).toContain(1);
+        expect(rangeInc(1, 1)).toContain(1);
+        expect(rangeInc(0, 10)).toContain(10);
+        expect(rangeInc(-10, 10)).toContain(10);
+        expect(rangeInc(-10, 0)).toContain(0);
+        expect(rangeInc(-10, -10)).toContain(-10);
     });
 });
