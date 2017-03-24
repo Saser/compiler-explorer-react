4e7b6509a60de42e5b12660fcfaf2f19afb28cd5
Christian Persson <saser@live.se>
2017-03-24 09:44:00 +0100

    Reorganize imports, and introduce redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+
 import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+
+import App from './components/App';
 import compilerExplorer from './reducers/compilerExplorer.js';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     compilerExplorer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(
+        applyMiddleware(thunkMiddleware)
+    )
 );
 
 ReactDOM.render(
