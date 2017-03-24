335fb714770b92aed0fc7c1f4f1b935dc5227f4d
Christian Persson <saser@live.se>
2017-03-24 17:31:32 +0100

    Implementation of `intersperse`

--- src/utils/ArrayUtils.js
+++ src/utils/ArrayUtils.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 // Returns an array of integers from `start` (inclusive) to `end` (exclusive).
 export const range = (start, end) => {
     if (start >= end) {
@@ -12,6 +14,13 @@ export const rangeInc = (start, end) => {
     return range(start, end + 1);
 }
 
+// Intersperses a `separator` between each element in `array`.
 export const intersperse = (separator, array) => {
-    return undefined;
+    if (array.length <= 1) {
+        return array;
+    }
+
+    const head = _.head(array);
+    const tail = _.tail(array);
+    return [head, separator].concat(intersperse(separator, tail));
 }
