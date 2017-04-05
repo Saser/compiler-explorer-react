bf994d98994d6765663e7564ed7ae207d9661a94
Christian Persson <saser@live.se>
2017-04-05 20:55:36 +0200

    Update sample JSON to one from the newest version of the compiler

--- src/containers/LangTrees.js
+++ src/containers/LangTrees.js
@@ -4,7 +4,7 @@ import PresLangTreeWrapper from '../components/preslang/PresLangTreeWrapper.js';
 
 const mapStateToProps = (state) => ({
     // TODO: the below is hardcoded, and should be changed in the future.
-    tree: state.trees.trees && state.trees.trees[0],
+    tree: state.trees.trees && state.trees.trees[0].prog,
 })
 
 const mapDispatchToProps = (dispatch) => ({
