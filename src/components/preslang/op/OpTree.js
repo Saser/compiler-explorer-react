ea131c3d7df60cd941bc938d4c6732988e9ed7b5
Christian Persson <saser@live.se>
2017-04-06 15:48:06 +0200

    Add top-level component for all operators, `OpTree`

---
+++ src/components/preslang/op/OpTree.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import Opn from './Opn.js';
+import Opb from './Opb.js';
+import Opw from './Opw.js';
+import Shift from './Shift.js';
+import WordFromInt from './WordFromInt.js';
+import WordToInt from './WordToInt.js';
+import FFI from './FFI.js';
+
+const simpleOp = (cons) => () => <span>{cons}</span>;
+
+const OpTree = ({ opTree }) => {
+    let component = undefined;
+
+    switch (opTree.cons) {
+        case 'Opn':
+            component = Opn;
+            break;
+        case 'Opb':
+            component = Opb;
+            break;
+        case 'Opw':
+            component = Opw;
+            break;
+        case 'Shift':
+            component = Shift;
+            break;
+        case 'WordFromInt':
+            component = WordFromInt;
+            break;
+        case 'WordToInt':
+            component = WordToInt;
+            break;
+        case 'FFI':
+            component = FFI;
+            break;
+        default:
+            component = simpleOp(opTree.cons);
+            break;
+    }
+}
