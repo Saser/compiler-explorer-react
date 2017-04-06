969a0fb0bf75855aed72f8517e3e6cbedd6d24ca
Christian Persson <saser@live.se>
2017-04-06 15:49:11 +0200

    Fix broken stuff with `Var_local`

--- src/components/preslang/exp/Var_local.js
+++ src/components/preslang/exp/Var_local.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 import ExpSpan from './ExpSpan.js';
 
-const Var_local = () => {
-    // TODO: the property cannot be named `var`, since that is a JavaScript
-    // reserved keyword.
+const Var_local = ({ varN }) => {
     return (
         <ExpSpan>
-            Var_local <span style={{ color: 'red' }}>FIX THIS</span>
+            Var_local {varN}
         </ExpSpan>
     );
 }
+
+Var_local.propTypes = {
+    varN: PropTypes.string.isRequired,
+};
 
 export default Var_local;
