31efa2107c4b37edf72e4e2c6247f1e3d53fa7b2
Christian Persson <saser@live.se>
2017-04-07 16:35:30 +0200

    Re-add the other components I have created

--- src/components/preslang/op/OpTree.js
+++ src/components/preslang/op/OpTree.js
@@ -1,12 +1,24 @@
 import React, { PropTypes } from 'react';
 
+import FFI from './FFI.js';
+import Opb from './Opb.js';
 import Opn from './Opn.js';
+import Opw from './Opw.js';
+import Shift from './Shift.js';
+import WordFromInt from './WordFromInt.js';
+import WordToInt from './WordToInt.js';
 
 import OpSpan from './OpSpan.js';
 import Nothing from '../Nothing.js';
 
 const opCons = [
+    'FFI',
+    'Opb',
     'Opn',
+    'Opw',
+    'Shift',
+    'WordFromInt',
+    'WordToInt',
 ];
 
 export const isOp = (cons) => opCons.includes(cons);
@@ -15,8 +27,26 @@ const OpTree = ({ opTree }) => {
     let component = undefined;
 
     switch (opTree.cons) {
+        case 'FFI':
+            component = FFI;
+            break;
+        case 'Opb':
+            component = Opb;
+            break;
         case 'Opn':
             component = Opn;
+            break;
+        case 'Opw':
+            component = Opw;
+            break;
+        case 'Shift':
+            component = Shift;
+            break;
+        case 'WordFromInt':
+            component = WordFromInt;
+            break;
+        case 'WordToInt':
+            component = WordToInt;
             break;
         case 'Opapp':
             component = () => <OpSpan>{opTree.cons}</OpSpan>;
