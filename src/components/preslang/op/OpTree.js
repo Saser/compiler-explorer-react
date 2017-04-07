005e34067783e77784956a9c00ae92101a616355
Christian Persson <saser@live.se>
2017-04-07 16:46:06 +0200

    Add the remaining "complex" components to `OpTree` as well

--- src/components/preslang/op/OpTree.js
+++ src/components/preslang/op/OpTree.js
@@ -1,6 +1,9 @@
 import React, { PropTypes } from 'react';
 
+import Chopb from './Chopb.js';
 import FFI from './FFI.js';
+import Init_global_var from './Init_global_var.js';
+import Op from './Op.js';
 import Opb from './Opb.js';
 import Opn from './Opn.js';
 import Opw from './Opw.js';
@@ -12,7 +15,10 @@ import OpSpan from './OpSpan.js';
 import Nothing from '../Nothing.js';
 
 const opCons = [
+    'Chopb',
     'FFI',
+    'Init_global_var',
+    'Op',
     'Opb',
     'Opn',
     'Opw',
@@ -27,8 +33,17 @@ const OpTree = ({ opTree }) => {
     let component = undefined;
 
     switch (opTree.cons) {
+        case 'Chopb':
+            component = Chopb;
+            break;
         case 'FFI':
             component = FFI;
+            break;
+        case 'Init_global_var':
+            component = Init_global_var;
+            break;
+        case 'Op':
+            component = Op;
             break;
         case 'Opb':
             component = Opb;
