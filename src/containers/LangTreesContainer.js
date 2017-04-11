c2e2dfc9640904dbf8a67e2943e99f860aa5bd78
Christian Persson <saser@live.se>
2017-04-11 11:13:15 +0200

    Remove outdated TODO comment

--- src/containers/LangTreesContainer.js
+++ src/containers/LangTreesContainer.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import PresLangTrees from '../components/preslang/PresLangTrees.js';
 
 const mapStateToProps = (state) => ({
-    // TODO: the below is hardcoded, and should be changed in the future.
     trees: state.trees.trees,
 })
 
