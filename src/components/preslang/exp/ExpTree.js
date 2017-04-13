cd2ce25d9266469eeee6fd7e4828eede5e40e1f8
Christian Persson <saser@live.se>
2017-04-13 16:38:35 +0200

    Upgrade to React 15.5 (#5)
    * Update dependency versions
    
    * PropTypes moved to its own package -- refactor accordingly

--- src/components/preslang/exp/ExpTree.js
+++ src/components/preslang/exp/ExpTree.js
@@ -1,4 +1,4 @@
-import { PropTypes } from 'react';
+import PropTypes from 'prop-types';
 
 import App from './App.js';
 import Dlet from './Dlet.js';
--- src/components/preslang/op/OpTree.js
+++ src/components/preslang/op/OpTree.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
 import Chopb from './Chopb.js';
 import FFI from './FFI.js';
