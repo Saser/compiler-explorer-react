f50ac011e354cb9d37022b22c2225e24c5572010
Christian Persson <saser@live.se>
2017-04-14 18:54:39 +0200

    Tests relating to simple constructors are passing now

--- src/components/structuredlang/Cons.js
+++ src/components/structuredlang/Cons.js
@@ -1,9 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Cons = ({ name, args, trace }) => (
-    <span>Cons: NYI</span>
-)
+import _ from 'lodash';
+import { intersperse } from '../../utils/ArrayUtils.js';
+
+import Item from './Item.js';
+
+const shouldSurround = (child) => child.args && child.args.length > 0;
+
+const renderAndMaybeSurround = (arg) => {
+    const rendered = Item(arg);
+
+    let arr = [];
+    if (shouldSurround(arg)) {
+        arr = ['(', rendered, ')'];
+    } else {
+        arr = [rendered];
+    }
+    return arr;
+}
+
+const Cons = ({ name, args, trace }) => {
+    let renderedArgs = [];
+    if (args.length > 0) {
+        const nestedWithoutSpaces = args.map(renderAndMaybeSurround);
+        const nestedWithSpaces = intersperse(' ', nestedWithoutSpaces);
+        const withSpaces = _.flatten(nestedWithSpaces);
+        renderedArgs = [' '].concat(withSpaces);
+    }
+
+    return (
+        <span>
+            {name}{renderedArgs}
+        </span>
+    );
+}
 
 Cons.propTypes = {
     name: PropTypes.string.isRequired,
