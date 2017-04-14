bd6d889805e71dd4e3b51bd2a0d9dbf35f60f848
Christian Persson <saser@live.se>
2017-04-14 17:20:55 +0200

    Create the helper functions, with dummy implementations

--- src/components/preslang/Item.js
+++ src/components/preslang/Item.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Item = ({ isTuple }) => (
-    <span>Nothing yet</span>
+const Tuple = ({ elements }) => (
+    <span>Tuple: NYI</span>
 )
+
+Tuple.propTypes = {
+    elements: PropTypes.array.isRequired,
+};
+
+const Cons = ({ name, args, trace }) => (
+    <span>Cons: NYI</span>
+)
+
+Cons.propTypes = {
+    name: PropTypes.string.isRequired,
+    args: PropTypes.array.isRequired,
+    trace: PropTypes.object,
+};
+
+const Item = (props) => {
+    if (props.isTuple) {
+        return Tuple(props);
+    }
+
+    return Cons(props);
+}
 
 // Stricter proptype-checking is done in the helper functions above, Tuple and
 // Cons.
