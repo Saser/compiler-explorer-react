34a0d74c2b4537e5a7ea5bc916bd8c4eae45b43a
Christian Persson <saser@live.se>
2017-04-15 14:31:07 +0200

    Implement <Item /> so that it "passes" the first four tests
    The tests are not "passing", but they return the correct HTML, so eh,
    whatever.

--- src/components/structuredlang/Item.js
+++ src/components/structuredlang/Item.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Item = ({ name, args, trace }) => (
-    <span>Item: not yet implemented</span>
-)
+import _ from 'lodash';
+import { intersperse } from '../../utils/ArrayUtils.js';
+
+import StructuredExp from './StructuredExp.js';
+
+const shouldSurround = (item) => item.args && item.args.length > 0;
+
+// This function is intended to be used as the `callback` argument in a call to
+// `Array.prototype.map`, hence the index argument.
+const renderAndMaybeSurround = (sExp, index) => {
+    const rendered = <StructuredExp sExp={sExp} key={index} />;
+
+    let arr = [];
+    if (shouldSurround(sExp)) {
+        arr = ['(', rendered, ')'];
+    } else {
+        arr = [rendered];
+    }
+
+    return arr;
+}
+
+const Item = ({ name, args, trace }) => {
+    const nestedRenderedArgs = args.map(renderAndMaybeSurround);
+    const nestedNameAndArgs = [name].concat(nestedRenderedArgs);
+    const nestedWithSpaces = intersperse(' ', nestedNameAndArgs);
+    const withSpaces = _.flatten(nestedWithSpaces);
+
+    return <span>{withSpaces}</span>;
+}
 
 Item.propTypes = {
     name: PropTypes.string.isRequired,
