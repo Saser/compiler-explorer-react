a5759abdcf348f07a3d07a530af32f679eca9ffe
Christian Persson <saser@live.se>
2017-04-15 14:43:32 +0200

    Implement <Tuple />, tests manually verified to "pass"

--- src/components/structuredlang/Tuple.js
+++ src/components/structuredlang/Tuple.js
@@ -3,9 +3,13 @@ import PropTypes from 'prop-types';
 
 import { intersperse } from '../../utils/ArrayUtils.js';
 
-const Tuple = ({ elements }) => (
-    <span>Tuple: not yet implemented</span>
-)
+import StructuredExp from './StructuredExp.js';
+
+const Tuple = ({ elements }) => {
+    const renderedElements = elements.map((element, index) => <StructuredExp sExp={element} key={index} />);
+    const withCommas = intersperse(', ', renderedElements);
+    return <span>({withCommas})</span>;
+}
 
 Tuple.propTypes = {
     elements: PropTypes.array.isRequired,
