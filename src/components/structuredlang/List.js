3811d4e8b8159d1c9bd7a8829714265225c75200
Christian Persson <saser@live.se>
2017-04-15 15:48:49 +0200

    Implement <List />, seems to be passing the tests

--- src/components/structuredlang/List.js
+++ src/components/structuredlang/List.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const List = ({ elements }) => (
-    <span>List: not yet implemented</span>
-)
+import { intersperse } from '../../utils/ArrayUtils.js';
+
+import StructuredExp from './StructuredExp.js';
+
+const List = ({ elements }) => {
+    const renderedElements = elements.map((element, index) => <StructuredExp sExp={element} key={index} />);
+    const withSemicolons = intersperse('; ', renderedElements);
+    return <span>[{withSemicolons}]</span>;
+}
 
 List.propTypes = {
     elements: PropTypes.array.isRequired,
